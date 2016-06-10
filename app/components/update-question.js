eaa2dbd93e7ffda37d32d1fa5461c4eb74cb4106
John <jldjld503@gmail.com>
2016-06-10 12:54:32 -0700

    update just wont work

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -7,6 +7,7 @@ export default Ember.Component.extend({
             this.set('updateQuestionForm', true);
         },
         updateQuestion(question) {
+
             var params = {
                 question: this.get('question'),
                 questioner: this.get('questioner'),
